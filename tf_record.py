233c927bb5e80a021ea8b7080fc79e7aa3aa4913
Arin Swadi <akswadi20@gmail.com>
2024-06-14 12:00:19 +0530

    Yes

--- tf_record.py
+++
@@ -1,5 +0,0 @@
-from roboflow import Roboflow
-rf = Roboflow(api_key="oZaEzhFkJIWcPCpAmVsH")
-project = rf.workspace("arin-swadi-0xhey").project("blood-fdwzc")
-version = project.version(2)
-dataset = version.download("tfrecord")
